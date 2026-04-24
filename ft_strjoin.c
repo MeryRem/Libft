a43a14cf2778d0daad7d1a7039318c348d58514d
María Redondo <125565666+MeryRem@users.noreply.github.com>
2026-04-24 09:38:49 +0200

    Create ft_strjoin.c

---
+++ ft_strjoin.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariredo <mariredo@student.42.malaga.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/23 11:05:40 by mariredo          #+#    #+#             */
+/*   Updated: 2026/04/23 18:28:09 by mariredo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	size_t	i;
+	size_t	j;
+	char	*res;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	res = malloc((len1 + len2 + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (i < len1)
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < len2)
+	{
+		res[i + j] = s2[j];
+		j++;
+	}
+	res[i + j] = '\0';
+	return (res);
+}
+/*
+#include <stdio.h>
+int	main (void)
+{
+	char s[4] = "holi";
+	char t[3] = "wis";
+	printf("%s ", ft_strjoin(s, t));
+}
+*/
