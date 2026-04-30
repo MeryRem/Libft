ea49740874bc880ee09d696bff8b2554ffa7534d
María Redondo <125565666+MeryRem@users.noreply.github.com>
2026-04-30 15:32:13 +0200

    Create ft_lstadd_back.c

---
+++ ft_lstadd_back.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariredo <mariredo@student.42.malaga.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/30 13:15:48 by mariredo          #+#    #+#             */
+/*   Updated: 2026/04/30 15:30:12 by mariredo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*aux;
+
+	if (!lst || !new || lst == NULL)
+		return ;
+	aux = *lst;
+	while (aux->next)
+		aux = aux->next;
+	aux->next = new;
+}
