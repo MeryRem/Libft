13e0dfa910e25eae5c3d8c7bd338c336e4c246c1
María Redondo <125565666+MeryRem@users.noreply.github.com>
2026-04-30 16:00:19 +0200

    Update ft_lstadd_back.c

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: mariredo <mariredo@student.42.malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/30 13:15:48 by mariredo          #+#    #+#             */
-/*   Updated: 2026/04/30 15:30:12 by mariredo         ###   ########.fr       */
+/*   Updated: 2026/04/30 15:55:55 by mariredo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,13 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*aux;
 
-	if (!lst || !new || lst == NULL)
+	if (!lst || !new)
 		return ;
+	if  (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
 	aux = *lst;
 	while (aux->next)
 		aux = aux->next;
