4f461a1dcdd35168da15c64a6daed15fdf8aedc9
María Redondo <125565666+MeryRem@users.noreply.github.com>
2026-04-30 15:30:50 +0200

    Create ft_lstsize.c

---
+++ ft_lstsize.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariredo <mariredo@student.42.malaga.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/30 12:29:19 by mariredo          #+#    #+#             */
+/*   Updated: 2026/04/30 13:05:39 by mariredo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int		count;
+	t_list	*aux;
+
+	count = 0;
+	aux = lst;
+	while (aux != NULL)
+	{
+		aux = aux->next;
+		count++;
+	}
+	return (count);
+}
